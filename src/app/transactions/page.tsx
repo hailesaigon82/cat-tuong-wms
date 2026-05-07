c0f062c8e29c5a6e038029ff10d97fa055a7cae9
hailesaigon82 <haile.saigon@gmail.com>
2026-05-07 09:44:32 +0700

    updated

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -5,5 +5,5 @@ import { useSearchParams } from "next/navigation";
 
 export default function TransactionsPage() {
   const searchParams = useSearchParams();
-  return <TransactionForm type="in" allowTypeSwitch autoOpenScanner={searchParams.get("scan") === "1"} />;
+  return <TransactionForm type="out" allowTypeSwitch autoOpenScanner={searchParams.get("scan") === "1"} />;
 }
